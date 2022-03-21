986fd3b2ef3203e898ac7beffa904a40e3f35849
FlorianGrimm <grimmuko@googlemail.com>
2022-03-21 10:24:47 +0100

    step

---
+++ samples/poc/src/PocLibrary/Entity/PocContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poc.Entity {
+    public class PocContext : DbContext {
+        public PocContext() {
+        }
+
+        public virtual DbSet<TodoEntity> Todo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<TodoEntity>(
+                todoEntity => {
+                    todoEntity.Property(e => e.Title);
+                }
+                );
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
+/*
+ dotnet ef dbcontext scaffold "Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -o Models
+ */
--- samples/poc/src/PocLibrary/Entity/TodoEntity.cs
+++ samples/poc/src/PocLibrary/Entity/TodoEntity.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Poc.Entity {
-    class TodoEntity {
+    public class TodoEntity {
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset ModifiedAt { get; set; }
     }
 }
